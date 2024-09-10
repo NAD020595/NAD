90f56a793ea54dc5b11a756c27326d63d2b1c3ba
Duy Nguyen <nguyenanhduy02051995@gmail.com>
2024-09-10 22:51:33 +0700

    Start to build Web API project base and the first API

---
+++ WebAPI/Data/NADContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class NADContext : DbContext
+    {
+        public NADContext(DbContextOptions<NADContext> options) : base(options) 
+        {
+        
+        }
+
+        #region DbSet
+        public DbSet<Book>? Books { get; set; }
+        #endregion
+    }
+}
