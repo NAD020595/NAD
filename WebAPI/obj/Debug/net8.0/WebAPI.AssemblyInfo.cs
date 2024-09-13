724a767261f775d14edaa89c81124790c4467f5f
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2024-09-13 14:45:36 +0700

    .

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc8e17b3d88e8dc161031f8d3366bd15b3ddc5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2156a682288aaf1ac257c4af93567cc6700bdf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
